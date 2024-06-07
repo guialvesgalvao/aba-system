bb4c6f84558d1ebfb93eb4600472ceb5c80d32de
Lucas Pedro da Hora <lucaspedro517@gmail.com>
2024-06-06 22:39:51 -0300

    ✨Feat: Adicionando Shadcn e Vitest para React.

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button />
+    </div>
+  );
+}
+
+export default Home;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    environment: "jsdom",
+
+    globals: true,
+    setupFiles: "./src/shared/tests/setup.js",
+  },
+});
